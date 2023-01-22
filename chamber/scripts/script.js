6c21897266ddad2d32c07c4e3eee6d74613ca1bf
AndreaGarzon~ <gar21082@byui.edu>
2023-01-22 01:28:07 -0500

    chamber adds

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -13,3 +13,31 @@ const naveg = document.querySelector(".nav-space");
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle("responsive")}, false);
 hambutton.addEventListener('click', () => {naveg.classList.toggle("wider")}, false);
+
+
+let todayVisit = Math.floor(Date.now() / 86400000);
+const span = document.querySelector("#visits");
+
+function setVisit() {
+    localStorage.setItem("visit", JSON.stringify(todayVisit));
+
+} 
+
+function getVisit () {
+    return JSON.parse(localStorage.getItem("visit"));
+
+}
+
+if (localStorage.getItem("visit")) {
+    let pastVisit = getVisit();
+    let numberOfVisit = todayVisit - pastVisit;
+    let message = `You visited us ${numberOfVisit} days ago`;
+    span.textContent = message;
+    setVisit();
+}
+else {
+    let message = "This is your first visit";
+    span.textContent = message;
+    setVisit();
+}
+
